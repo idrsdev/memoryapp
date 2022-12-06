4bb543cbcccd3a7c38cf19e94c421dee2ea122ec
Idris <115788425+idrsdev@users.noreply.github.com>
2022-12-06 16:50:01 +0500

    reset-password refactoring

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import memoryRoutes from "./routes/memoryRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import commentRoutes from "./routes/commentRoutes.js";
+import passwordResetRoutes from "./routes/passwordResetRoutes.js";
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use("/api/user", userRoutes);
 app.use("/api/memory", memoryRoutes);
 app.use("/api/comment", commentRoutes);
+app.use("/api/reset-password", passwordResetRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,8 +4,6 @@ import {
   authUser,
   registerUser,
   resendAccountActivationEmail,
-  resetPassword,
-  resetPasswordEmail,
   verifyUser,
 } from "../controllers/userController.js";
 
@@ -19,8 +17,6 @@ const router = express.Router();
 router.route("/").post(validateNewUser, registerUser);
 router.route("/login").post(validateAuthCredentials, authUser);
 router.route("/verify/:email/:token").get(verifyUser);
-router.route("/reset-password").post(resetPasswordEmail);
-router.route("/reset-password/:userId/:token").post(resetPassword);
 router.route("/resend-activation-link").post(resendAccountActivationEmail);
 
 export default router;
